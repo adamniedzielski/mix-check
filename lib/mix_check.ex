a4572f612b56d9cb0822746696ebff11354e9a3b
Adam Niedzielski <adamsunday@gmail.com>
2017-11-14 19:46:46 +0100

    init

---
+++ lib/mix_check.ex
@@ -0,0 +1,18 @@
+defmodule MixCheck do
+  @moduledoc """
+  Documentation for MixCheck.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixCheck.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_check_test.exs
@@ -0,0 +1,8 @@
+defmodule MixCheckTest do
+  use ExUnit.Case
+  doctest MixCheck
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
